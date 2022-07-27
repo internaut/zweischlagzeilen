90b099edcf3c5f7966e7aed4987d24df651be68e
Markus Konrad <post@mkonrad.net>
2022-07-27 14:17:48 +0200

    fix zdf rss url

--- datasources.py
+++ datasources.py
@@ -3,7 +3,7 @@ import feedparser
 
 feeds = {
     'ard': 'http://www.tagesschau.de/xml/rss2',
-    'zdf': 'http://www.heute.de/?view=rss',
+    'zdf': 'https://www.zdf.de/rss/zdf/nachrichten',
     'spon': 'http://www.spiegel.de/schlagzeilen/index.rss',
     'sz': 'http://rss.sueddeutsche.de/app/service/rss/alles/index.rss?output=rss',
     'zeit': 'http://newsfeed.zeit.de/index',
